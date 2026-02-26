988744d74c21aefaa68507cc8faa9eaead77650b
TemodoJintaka <Douglasmoraperez203@gmail.com>
2026-02-26 12:01:06 -0400

    Remove Celery-related configurations and tasks from the project, including Docker configurations, settings, and task definitions. Update the README to reflect the removal of Celery functionality.

--- madera/ventas/models.py
+++ madera/ventas/models.py
@@ -6,6 +6,6 @@ class Lead(models.Model):
     client_name = models.CharField(max_length=100)
     whatsapp = models.CharField(max_length=20)
     # Relacionamos el lead con el producto que le gustó
-    product_interest = models.ManyToManyField('catalog.Product', on_delete=models.SET_NULL, null=True)
+    product_interest = models.ManyToManyField('catalog.Product', blank=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
